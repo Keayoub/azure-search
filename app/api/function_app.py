060806f25ac06b505cf0205f8fae350164fd7262
Ayoub KEBAILI <keayoub@msn.com>
2023-09-25 19:08:44 -0400

    change function prepare_docs name

--- app/api/function_app.py
+++ app/api/function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 @app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def timer_trigger(myTimer: func.TimerRequest) -> None:
+def prepare_docs(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
